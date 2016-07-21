a27a4add9a72c2c91c936cf9e92f415fbd7eb64c
Shunsuke <os18700241@gmail.com>
2016-07-21 17:25:04 +0900

    銀振申請　表示

--- app/controllers/admin/deposit_requests_controller.rb
+++ app/controllers/admin/deposit_requests_controller.rb
@@ -1,6 +1,7 @@
 module Admin
   class Admin::DepositRequestsController < BaseController
     def index
+      @deposit_requests = Default::DepositRequest.order(created_at: :desc)
     end
   end
 end
