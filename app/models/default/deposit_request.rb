a095ecaf414746d2a49b6c89b4ff9c7e5e1858b8
Shunsuke <os18700241@gmail.com>
2016-07-23 09:32:08 +0900

    入金履歴と入金申請の紐付け、数値にカンマ付け

--- app/models/default/deposit_request.rb
+++ app/models/default/deposit_request.rb
@@ -1,4 +1,4 @@
 class Default::DepositRequest < ActiveRecord::Base
   belongs_to :user, class_name: 'Default::User'
-  has_one :deposit_record, class_name: 'Admin::DepositRecord'
+  has_one :deposit_record, class_name: 'Admin::DepositRecord', foreign_key: 'deposit_request_id'
 end
