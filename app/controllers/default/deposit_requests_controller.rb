e6be7dbffcbeac34f7f52e1749e8b062eec34a32
Shunsuke <os18700241@gmail.com>
2016-07-28 22:29:11 +0900

    銀振り申請 レイアウト修正

--- app/controllers/default/deposit_requests_controller.rb
+++ app/controllers/default/deposit_requests_controller.rb
@@ -1,6 +1,7 @@
 module Default
   class Default::DepositRequestsController < BaseController
     skip_before_action :user_logged_in?
+    before_action :set_user, only: :new
 
 
     def new
@@ -23,8 +24,17 @@ module Default
 
 
     private
+      def set_user
+        # @change_user = Default::User.find(params[:id])
+        @user = Default::User.find(1)
+      end
       def default_deposit_request_params
-        params.require(:default_deposit_request).permit(:user_id, :amount, :status, :request_point)
+        params.require(:default_deposit_request).permit(
+          :user_id,
+          :amount,
+          :status,
+          :request_point
+        )
       end
   end
 end
